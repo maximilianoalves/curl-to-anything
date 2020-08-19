6d2244fcb608dda5210d5c62376077fb8fb48b97
Maximiliano <maximilianodacruz@gmail.com>
2020-08-19 17:18:56 -0300

    add support others args for curl commands

--- src/rules/curlverter.core.ts
+++ src/rules/curlverter.core.ts
@@ -3,32 +3,54 @@ export default class CurlverterCore {
     curl: string;
     url?: string;
     method?: string;
-    body?: string
+    body: string
     headers: {name: string, value: string }[]
 
     constructor(curl: string) {
         this.curl = curl;
-        this.headers = []
+        this.headers = [];
+        this.body = "";
         this.converter()
     }
 
     converter() {
-        let curlSplited = this.curl.split("--");
-        curlSplited.forEach( (value) => {
-            if (value.includes("header")) {
-                let header = value.split("'");
-                let headerValues: string[] = header[1].split(":");
-                this.headers.push({name: headerValues[0], value: headerValues[1]})
-            }
-            if (value.includes("data-raw")) {
-                let bodySplited = value.split("'");
-                this.body = bodySplited[1];
-            }
-            if (value.includes("request")) {
-                let request = value.split(" ");
-                this.method = request[1];
-                this.url = request[2];
-            }
-        })
+        if (this.curl.includes("--")) {
+            let curlSplited = this.curl.split("--");
+            curlSplited.forEach( (value) => {
+                if (value.includes("header")) {
+                    let header = value.split("'");
+                    let headerValues: string[] = header[1].split(":");
+                    this.headers.push({name: headerValues[0], value: headerValues[1]})
+                }
+                if (value.includes("data-raw")) {
+                    let bodySplited = value.split("'");
+                    this.body = bodySplited[1];
+                }
+                if (value.includes("request")) {
+                    let request = value.split(" ");
+                    this.method = request[1];
+                    this.url = request[2];
+                }
+            })
+        } else {
+            let curlSplited = this.curl.split(" -");
+            curlSplited.forEach( (value) => {
+                if (value.includes("H")) {
+                    let header = value.split("\"");
+                    let headerValues: string[] = header[1].split(":");
+                    this.headers.push({name: headerValues[0], value: headerValues[1]})
+                }
+                if (value.includes("d")) {
+                    let bodySplited = value.split(" \"");
+                    this.body = bodySplited[1];
+                }
+                if (value.includes("X")) {
+                    let request = value.split(" ");
+                    this.method = request[1];
+                    this.url = request[2];
+                }
+            })
+        }
+        
     }
 }
