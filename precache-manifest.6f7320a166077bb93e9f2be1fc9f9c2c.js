bb171839459659dc48a6e2fb53b58d38ff6f16c9
maximilianoalves <maximilianodacruz@gmail.com>
2020-08-20 21:11:21 +0000

    Deploying to gh-pages from master 8e626d9459c7a5c81927585210de98694dcd5033

---
+++ precache-manifest.6f7320a166077bb93e9f2be1fc9f9c2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e984b6e596d6d2cbfc899132f6a4c5",
+    "url": "/curl-to-anything/index.html"
+  },
+  {
+    "revision": "22cffbd5b412f89dc42a",
+    "url": "/curl-to-anything/static/css/main.b3d73f2e.chunk.css"
+  },
+  {
+    "revision": "dfb3d3882beb026f8cf9",
+    "url": "/curl-to-anything/static/js/2.35764649.chunk.js"
+  },
+  {
+    "revision": "e4feac4044798794c71a8902011e2edf",
+    "url": "/curl-to-anything/static/js/2.35764649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22cffbd5b412f89dc42a",
+    "url": "/curl-to-anything/static/js/main.66f138b6.chunk.js"
+  },
+  {
+    "revision": "61462c07557ed9a61543",
+    "url": "/curl-to-anything/static/js/runtime-main.15999a3f.js"
+  }
+]);
